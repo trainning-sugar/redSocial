2c396a32050da5b132ba33f46ca689df9de80593
trainning-sugar <mlvilchezp@gmail.com>
2018-07-19 05:23:57 -0500

    agregando las funciones para los test

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-//var rootRef = firebase.database().ref();
-
-// Dom Elements
 const buttonSend = document.getElementById('btnSend');
+const rootRef = firebase.database().ref();
 
-document.addEventListener('DOMContentLoaded', () => {
-  const rootRef = firebase.database().ref();
+const render = obj => {
+  const tbody = document.getElementById('tbody-data');
+  tbody.innerHTML = '';
+  for (let property in obj) {
+    tbody.innerHTML += `
+            <tr>
+            <th scope="row">${property}</th>
+            <td>${obj[property].mail}</td>
+            <td>${obj[property].number}</td>
+            <td>${obj[property].post}</td>
+            </tr>
+          `
+  }
+};
 
+
+window.getUsers = (rootRef) => {
   rootRef.once('value')
     .then(snapshot => {
       const data = snapshot.val();
-      const tbody = document.getElementById('tbody-data');
-      tbody.innerHTML = '';
-
-      for (let property in data) {
-        tbody.innerHTML += `
-      <tr>
-      <th scope="row">${property}</th>
-      <td>${data[property].mail}</td>
-      <td>${data[property].number}</td>
-      <td>${data[property].post}</td>
-    </tr>
-      `
-      }
+      render(data);
     })
     .catch(error => {
       console.log('error' + error);
     });
+}
 
 
-  buttonSend.addEventListener('click', e => {
-    e.preventDefault();
-    const name = document.getElementById('inputName').value;
-    const dataUser = {
-      name,
-      mail: document.getElementById('inputEmail').value,
-      number: document.getElementById('inputAge').value,
-      post: document.getElementById('inputComment').value
-    }
+window.createUser = (rootRef, name, dataUser) => {
+  rootRef.once('value')
+    .then(snapshot => {
+      if (snapshot.hasChild(name)) {
+        alert('El dato ya existe en la base de datos');
+        document.getElementById('inputName').value = '';
+        document.getElementById('inputEmail').value = '';
+        document.getElementById('inputAge').value = '';
+        document.getElementById('inputComment').value = '';
+      } else {
+        alert('se grabaron los datos')
+        rootRef.child(name).set(dataUser);
+        document.getElementById('inputName').value = '';
+        document.getElementById('inputEmail').value = '';
+        document.getElementById('inputAge').value = '';
+        document.getElementById('inputComment').value = '';
+      }
+    })
+    .catch(error => {
+      console.log('error' + error);
+    })
+}
 
-    rootRef.once('value')
-      .then(snapshot => {
-        if (snapshot.hasChild(name)) {
-          alert('El dato ya existe en la base de datos');
-          document.getElementById('inputName').value = '';
-          document.getElementById('inputEmail').value = '';
-          document.getElementById('inputAge').value = '';
-          document.getElementById('inputComment').value = '';
-        } else {
-          alert('se grabaron los datos')
-          rootRef.child(name).set(dataUser);
-          document.getElementById('inputName').value = '';
-          document.getElementById('inputEmail').value = '';
-          document.getElementById('inputAge').value = '';
-          document.getElementById('inputComment').value = '';
-        }
-      })
-      .catch(error => {
-        console.log('error' +  error);
-      })
-
-    /*   rootRef.once('value', snapshot => {
-        if (snapshot.hasChild(name)) {
-          document.getElementById('inputName').value = '';
-          document.getElementById('inputEmail').value = '';
-          document.getElementById('inputAge').value = '';
-          document.getElementById('inputComment').value = '';
-
-          alert('El dato ya existe en la base de datos');
-        } else {
-          rootRef.child(name).set(dataUser);
-
-        }
-      }); */
+const addUsers = e => {
+  e.preventDefault();
+  const name = document.getElementById('inputName').value;
+  const dataUser = {
+    name,
+    mail: document.getElementById('inputEmail').value,
+    number: document.getElementById('inputAge').value,
+    post: document.getElementById('inputComment').value
+  };
+  createUser(name, dataUser);
+};
 
 
-  })
-})
+document.addEventListener('DOMContentLoaded', getUsers);
+buttonSend.addEventListener('click', addUsers);
