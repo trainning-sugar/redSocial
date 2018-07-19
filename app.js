6a6d97180e4d3cc029357d4c1f25e6a6dc54733a
trainning-sugar <mlvilchezp@gmail.com>
2018-07-19 03:42:11 -0500

    agregando firebase y consultando data

---
+++ app.js
@@ -0,0 +1,23 @@
+//var rootRef = firebase.database().ref();
+
+const rootRef = firebase.database().ref();
+rootRef.once("value")
+  .then(snapshot=> {
+    const data = snapshot.val();
+    const tbody = document.getElementById('tbody-data');
+    tbody.innerHTML = '';
+
+    for(let property in data){
+      tbody.innerHTML  += `
+      <tr>
+      <th scope="row">${property}</th>
+      <td>${data[property].mail}</td>
+      <td>${data[property].number}</td>
+      <td>${data[property].post}</td>
+    </tr>
+      ` 
+    }
+  })
+  .catch(error=>{
+    console.log('error' + error);
+  });
